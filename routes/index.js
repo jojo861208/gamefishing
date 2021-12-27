7167726b14d62b29733ce438092080b3b5ce821f
Peter <peter77553321@gmail.com>
2021-12-27 15:37:51 +0800

    SQL connection in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b8228822a37ac5',
+  password: 'e26c1c18',
+  database: 'heroku_e783206b1d51501',
+  port: 3306
+});
+
+conn.connect();
+
+conn.query('SELECT client_id FROM group_info', function (err, rows, fields) {
+  if (err) throw err;
+  console.log('The result is: ', rows[0]);
+});
+conn.end();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
