ab96a435cfed07e70c3b12d2f62a9ca7254bb5b6
Peter <peter77553321@gmail.com>
2021-12-30 17:50:45 +0800

    mysql update

--- routes/index.js
+++ routes/index.js
@@ -19,16 +19,17 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/query_groupinfo', function (req, res, next) {
-  var sql = group_info;
-  mysqlPoolQuery('SELECT client_id FROM ?', sql, function (err, result) {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ success: "False", message: "資料庫讀取失敗:\n" });
-    } else {
-      console.log("讀取資料庫成功")
-      console.log(result)
-    }
-  });
-})
+//remain fixing
+// router.post('/query_groupinfo', function (req, res, next) {
+//   var sql = group_info;
+//   mysqlPoolQuery('SELECT client_id FROM ?', sql, function (err, result) {
+//     if (err) {
+//       console.log(err);
+//       return res.status(500).json({ success: "False", message: "資料庫讀取失敗:\n" });
+//     } else {
+//       console.log("讀取資料庫成功")
+//       console.log(result)
+//     }
+//   });
+// })
 module.exports = router;
