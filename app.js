defefa6602d6cf18c5bfb97d8b5b0b6d0c02ca0b
cyje66 <eddie71677@gmail.com>
2021-12-30 16:52:44 +0800

    add socket function

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const http = require("http");
+const io = require('socket.io')(http);
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,6 +9,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var port  = 3001;
+var server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`socket server listen at: ${port}`);
+})
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +23,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var servIo = io.listen(server);
+servIo.on('connection', function (socket) {
+    console.log('user connected!');
+    setInterval(function () {
+        socket.emit('second', { 'second': new Date().getSeconds() });
+    }, 1000);
+    socket.on('client_data', function (data) {
+        console.log(data);
+    });
+    socket.on('disconnect', function() {
+        console.log('a user disconnected!');
+    });
+});
 
 module.exports = app;
