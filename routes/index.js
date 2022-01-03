0ed55a19651ff6053161ca8e561af252b19ec7c0
Peter <peter77553321@gmail.com>
2022-01-03 18:49:53 +0800

    query API

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var mysql = require('mysql');
 var { mysqlPoolQuery } = require('../connection/mysql.js')
 
+var client = "peter";
 
-mysqlPoolQuery('SELECT client_id FROM group_info', function (err, result) {
+mysqlPoolQuery('SELECT * FROM group_info WHERE client_id = ?', [client], function (err, result) {
   if (err) {
     console.log(err);
     return res.status(500).json({ success: "False", message: "資料庫讀取失敗:\n" });
@@ -17,7 +18,12 @@ mysqlPoolQuery('SELECT client_id FROM group_info', function (err, result) {
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+
 });
+
+router.get('/query1', function (req, res, next) {
+  console.log("屁眼")
+})
 
 //remain fixing
 // router.post('/query_groupinfo', function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var { mysqlPoolQuery } = require('../connection/mysql.js')
+
+// mysqlPoolQuery('SELECT * FROM group_info WHERE client_id = ?', [client], function (err, result) {
+//   if (err) {
+//     console.log(err);
+//     return res.status(500).json({ success: "False", message: "資料庫讀取失敗:\n" });
+//   } else {
+//     console.log("讀取資料庫成功")
+//     console.log(result)
+//   }
+// });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.post('/query_group_info', function (req, res, next) {
+  var client_id = req.body.client_id;
+  console.log('This is group_info query API');
+
+  mysqlPoolQuery('SELECT * FROM group_info WHERE client_id = ?', [client_id], function (err, result) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ success: "False", message: "資料庫讀取失敗:\n" });
+    } else {
+      console.log("讀取資料庫成功")
+      console.log(result)
+      return res.status(200).json({ success: "success", message: result });
+
+    }
+  });
+})
 
 module.exports = router;
