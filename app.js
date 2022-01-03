11041ce69c97062baf44d32ac96d9faef1bb5321
Peter <peter77553321@gmail.com>
2022-01-03 21:19:26 +0800

    嘻嘻 趕快pull唷

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,9 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
 app.use('/users', usersRouter);
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 
 // socket server listening to port 3001
