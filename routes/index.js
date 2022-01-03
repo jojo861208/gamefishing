9b53543320a6b4228631a95d3c184972222eadec
Peter <peter77553321@gmail.com>
2022-01-03 18:57:40 +0800

    query update

--- routes/index.js
+++ routes/index.js
@@ -3,39 +3,10 @@ var router = express.Router();
 var mysql = require('mysql');
 var { mysqlPoolQuery } = require('../connection/mysql.js')
 
-var client = "peter";
-
-mysqlPoolQuery('SELECT * FROM group_info WHERE client_id = ?', [client], function (err, result) {
-  if (err) {
-    console.log(err);
-    return res.status(500).json({ success: "False", message: "資料庫讀取失敗:\n" });
-  } else {
-    console.log("讀取資料庫成功")
-    console.log(result)
-  }
-});
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 
 });
 
-router.get('/query1', function (req, res, next) {
-  console.log("屁眼")
-})
-
-//remain fixing
-// router.post('/query_groupinfo', function (req, res, next) {
-//   var sql = group_info;
-//   mysqlPoolQuery('SELECT client_id FROM ?', sql, function (err, result) {
-//     if (err) {
-//       console.log(err);
-//       return res.status(500).json({ success: "False", message: "資料庫讀取失敗:\n" });
-//     } else {
-//       console.log("讀取資料庫成功")
-//       console.log(result)
-//     }
-//   });
-// })
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,14 @@ var { mysqlPoolQuery } = require('../connection/mysql.js')
 //   } else {
 //     console.log("讀取資料庫成功")
 //     console.log(result)
+//     return res.status(200).json({ success: "success", message: result });
+
 //   }
 // });
 
 /* GET users listing. */
 
+//查詢group_info此table可用此api
 router.post('/query_group_info', function (req, res, next) {
   var client_id = req.body.client_id;
   console.log('This is group_info query API');
@@ -27,6 +30,7 @@ router.post('/query_group_info', function (req, res, next) {
       console.log("讀取資料庫成功")
       console.log(result)
       return res.status(200).json({ success: "success", message: result });
+      //回傳為json格式，可以透過物件的方式調用裡面資料
 
     }
   });
